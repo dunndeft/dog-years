07995753a19f0f46cfc938a315c0be5b96d49a39
Zane Dunn <60718015+dunndeft@users.noreply.github.com>
2021-11-26 21:25:36 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+// My current age.
+const myAge = 33;
+
+// The value saved to this variable will change.
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears *= 4;
+
+// console.log(earlyYears);
+// console.log(laterYears);
+
+let myAgeInDogYears = earlyYears + laterYears;
+
+let myName = 'Zane'.toLowerCase();
+
+// Your name and age in dog years.
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
